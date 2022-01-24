29c76e55f4f0c1116591813f9ac46d7f40ca2d85
Arda Örkin <ardaorkin@gmail.com>
2022-01-25 00:10:34 +0300

    category checked has been added

--- routes/post.js
+++ routes/post.js
@@ -1,15 +1,24 @@
 var express = require("express");
+const {
+  default: categoryController,
+} = require("../controllers/categoryController");
 const { default: Books } = require("../utils/Books");
 var router = express.Router();
 
 router.post("/books", async function (req, res, next) {
-  const books = new Books({ ...req.body });
-  const newBook = await books.createBook();
-  if (newBook) {
-    res.json(newBook);
-  } else {
-    res.sendStatus(400);
-  }
+  return await categoryController(req.body?.category)
+    .then(async (result) => {
+      const books = new Books({ ...req.body });
+      const newBook = await books.createBook();
+      if (newBook) {
+        return res.json(newBook);
+      } else {
+        return res.sendStatus(400);
+      }
+    })
+    .catch((error) => {
+      res.send(error.message);
+    });
 });
 
 module.exports = router;
